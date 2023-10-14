3f64ee2671c4b45864ec1dd5a26a2d43b7fed365
BharathSBSKR <sbharathsaikumarreddy@gmail.com>
2023-10-14 16:59:45 +0530

    first commit

---
+++ src/components/Clubs.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Clubs = () => {
+  return (
+    <section>
+            <h2>Clubs & Organizations</h2>
+            <ul>
+                <li>IEEE MTTS (Design) - Team Member - 2021</li>
+            </ul>
+    </section>
+  );
+}
+
+export default Clubs;
+
---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section>
+            <h2>Projects</h2>
+            <h3>Forms</h3>
+            <p>Created Forms to demonstrate proficiency in HTML and CSS by creating a functional and aesthetically pleasing E-commerce website for a furniture showroom. It showcases the ability to design and build web interfaces that prioritize user experience, product presentation, and accessibility.</p>
+    </section>
+  );
+}
+
+export default Projects;
+
